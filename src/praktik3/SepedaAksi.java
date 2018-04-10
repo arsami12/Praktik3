6f4f33dc0bb7a667dc590e3a77474701155d4d89
kendi ardi <arsamisapper12@gmail.com>
2018-04-10 08:48:02 +0800

    Instance, buat atribut dan panggil method

--- src/praktik3/SepedaAksi.java
+++ src/praktik3/SepedaAksi.java
@@ -1,4 +1,11 @@
 package praktik3;
 public class SepedaAksi {
-    
+    public static void main(String[] args) {
+        Sepeda s = new Sepeda();
+        s.merk="Polygon";
+        s.warna="Biru";
+        s.harga=1000;
+        
+        s.cetakInfo();
+    }
 }
