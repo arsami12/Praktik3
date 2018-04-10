578766a6434bf96592b73fed2c649c4bcba24d5c
kendi ardi <arsamisapper12@gmail.com>
2018-04-10 08:57:22 +0800

    Menggunakan Method Set

--- src/praktik3/SepedaAksi.java
+++ src/praktik3/SepedaAksi.java
@@ -2,9 +2,10 @@ package praktik3;
 public class SepedaAksi {
     public static void main(String[] args) {
         Sepeda s = new Sepeda();
-        s.merk="Polygon";
-        s.warna="Biru";
-        s.harga=1000;
+
+        s.setMerk("Polygon");
+        s.setWarna("Biru");
+        s.setHarga(1000);
         
         s.cetakInfo();
     }
